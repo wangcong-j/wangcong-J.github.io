4a3ce8306fa570c37a52a996371dc94dcaa0a28e
unknown <you@example.com>
2019-09-20 10:18:45 +0800

    Site updated: 2019-09-20 10:18:44

--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
-// navbar burger
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function () {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  // Check if there are any nav burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', () => {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
 
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+      });
+    });
+  }
+   var sr  = document.getElementById('search');
+  (window.screen.width < 800) ? sr.style.width= "" :  sr.style.width= "300px";
 
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-
-  });
 });
 
-// preloader
-window.addEventListener('load',function(){
-  document.querySelector('body').classList.add("loaded")  
-});
+
+var value = document.getElementById("cur").value;
+if (value !== ""){
+    var u  = document.getElementById('share');
+    u.onmouseover = function() {
+        document.getElementById("shared").style.display = 'block';
+    }
+    u.onmouseout = function() {
+        document.getElementById("shared").style.display = 'none';
+    }
+    var al = document.getElementById('shared').getElementsByTagName('a');
+    for (var i=0; i<al.length; i++){
+        al[i].setAttribute('target', '_blank');
+    }
+    document.getElementById('fb').setAttribute("href", "https://www.facebook.com/sharer.php?u="+value);
+    document.getElementById('tw').setAttribute("href", "https://twitter.com/intent/tweet?url="+value);
+    document.getElementById('gp').setAttribute("href", "https://plus.google.com/share?url="+value);
+    document.getElementById('pt').setAttribute("href", "http://pinterest.com/pin/create/button/?url="+value);
+}
+
